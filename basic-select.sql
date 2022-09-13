9e1050deafd46b3977d8f9fdbdfc69734a116a81
Shirlyn Chua <113419639+ShirlynChua@users.noreply.github.com>
2022-09-14 00:16:53 +0800

    Create basic-select.sql

---
+++ basic-select.sql
@@ -0,0 +1,12 @@
+/* 
+
+HackerRank Basic Select Challenges & My Solutions
+
+Skills used: SQL (Basic)
+
+*/
+
+/* 
+Revising the Select Query I
+Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+The CITY table is described as follows:
