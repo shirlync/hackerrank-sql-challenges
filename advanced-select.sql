ed2c227396be48b9dde70278613e975ed4c81686
Shirlyn Chua <113419639+ShirlynChua@users.noreply.github.com>
2022-09-14 22:11:30 +0800

    Update advanced-select.sql

--- advanced-select.sql
+++ advanced-select.sql
@@ -23,4 +23,22 @@ SELECT
     END tuple
 FROM TRIANGLES;
 
+/*
+Type of Triangle
+Generate the following two result sets:
+Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format: 
+There are a total of [occupation_count] [occupation]s.
+where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
+*/
+SELECT CONCAT(name, '(', LEFT(occupation,1), ')') 
+FROM OCCUPATIONS ORDER BY name ASC;
+
+-- s. is used to denote string
+SELECT 'There are a total of', (COUNT(occupation)), CONCAT(LOWER(occupation), 's.') 
+FROM OCCUPATIONS GROUP BY occupation ORDER BY count(occupation) ASC, occupation ASC;
+-- OR (There are 2 solutions for 2nd part of the question)
+SELECT CONCAT('There are a total of ', COUNT(occupation), ' ', LOWER(occupation), 's.') 
+FROM OCCUPATIONS GROUP BY occupation ORDER BY count(occupation) ASC, occupation ASC;
+
 /* TO BE CONTINUE */
