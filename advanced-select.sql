8f82f5523135285141ee0b3f700b5a26cf62915f
Shirlyn Chua <113419639+ShirlynChua@users.noreply.github.com>
2022-09-15 11:52:10 +0800

    Update advanced-select.sql

--- advanced-select.sql
+++ advanced-select.sql
@@ -41,4 +41,36 @@ FROM OCCUPATIONS GROUP BY occupation ORDER BY count(occupation) ASC, occupation 
 SELECT CONCAT('There are a total of ', COUNT(occupation), ' ', LOWER(occupation), 's.') 
 FROM OCCUPATIONS GROUP BY occupation ORDER BY count(occupation) ASC, occupation ASC;
 
+/*
+Occupations
+Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
+Note: Print NULL when there are no more names corresponding to an occupation.
+Occupation will only contain one of the following values: Doctor, Professor, Singer or Actor.
+The first column is an alphabetically ordered list of Doctor names. 
+The second column is an alphabetically ordered list of Professor names. 
+The third column is an alphabetically ordered list of Singer names. 
+The fourth column is an alphabetically ordered list of Actor names. 
+The empty cell data for columns with less than the maximum number of names per occupation (in this case, the Professor and Actor columns) are filled with NULL values.
+*/
+SELECT
+    [Doctor],
+    [Professor],
+    [Singer],
+    [Actor]
+FROM 
+    (SELECT 
+-- Row_number is the Num.1,2,3, giving a number to each row
+-- Over is just a clause to combine both
+-- Partition_by is partitioning the query
+        ROW_NUMBER() OVER (PARTITION BY occupation ORDER BY name) rn,
+        [name],
+        [occupation]
+     FROM
+        OCCUPATIONS
+     ) AS source
+PIVOT
+-- Only pull out Max to avoid NULL values
+    (MAX(name) FOR [occupation] IN ([Doctor], [Professor], [Singer], [Actor])) as pvt
+ORDER by rn
+
 /* TO BE CONTINUE */
